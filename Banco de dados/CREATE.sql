275d6a7b136a80edcfcf619e670160c8ee29c8f7
Leopls <leonardobento233@gmail.com>
2023-04-21 15:48:06 -0300

    Acertos no banco de dados

--- Banco de dados/CREATE.sql
+++ Banco de dados/CREATE.sql
@@ -1,18 +1,15 @@
 CREATE DATABASE emoove;
-
 USE emoove;
 
-
+drop database emoove;
 
 -- CREATE DA TABELA DO USUARIO QUE VAI CONTRATAR
 CREATE TABLE usuario(
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nomeUsuario VARCHAR(50) NOT NULL,
     sobrenome VARCHAR(50) NOT NULL,
-    cpf CHAR(14) NOT NULL,
     email VARCHAR(100) NOT NULL,
 	senha VARCHAR(50) NOT NULL,
-    cargo_funcionario VARCHAR(30),
     fk_estabelecimento INT,
     FOREIGN KEY (fk_estabelecimento) REFERENCES estabelecimento (idEstabelecimento)
 );
@@ -20,8 +17,7 @@ CREATE TABLE usuario(
 
 CREATE TABLE telefoneUsuario(
 	idTelefone INT PRIMARY KEY AUTO_INCREMENT,
-    telefoneFixo CHAR(12),
-    celular CHAR(13) NOT NULL,
+    celular VARCHAR(20) NOT NULL,
     fk_usuario INT,
     FOREIGN KEY (fk_usuario) REFERENCES Usuario (idUsuario)
 );
@@ -40,9 +36,9 @@ CREATE TABLE estabelecimento(
 
 CREATE TABLE telefoneEstabelecimento(
 	idTelefone INT PRIMARY KEY AUTO_INCREMENT,
-    telefoneAtendimento CHAR(12) NOT NULL,
-    telefoneSAQ CHAR(12),
-    celular CHAR(12),
+    telefoneAtendimento CHAR(10) NOT NULL,
+    telefoneSAQ varCHAR(20),
+    celular varCHAR(20),
     fk_estabelecimento INT,
     FOREIGN KEY (fk_estabelecimento) REFERENCES Estabelecimento (idEstabelecimento)
 );
@@ -78,12 +74,11 @@ CREATE TABLE Funcionario(
 
 -- CREATE DA TABELA QUE VAI ARMAZENAR OS DADOS CAPTURADOS PELO SENSOR
 CREATE TABLE CapturaDados(
-	idCaptura INT,
-	valor BOOLEAN,   
 	dtHora  DATETIME DEFAULT current_timestamp,
-	fk_sensor INT,
+    fk_sensor INT,
 	FOREIGN KEY (fk_Sensor) REFERENCES Sensor(idSensor),
-    constraint pkComposta PRIMARY KEY (idCaptura, fk_sensor)
+    constraint pkComposta PRIMARY KEY (dtHora, fk_sensor),
+	valor BOOLEAN
 );
 
 -- INSERT TABELA USUARIO
@@ -94,10 +89,10 @@ INSERT INTO usuario values
 	(null, 'Renan', 'Santos', '33333333333', 'renan@gmail.com', '123',null, 3);
     
 INSERT INTO  telefoneUsuario values
-	(null, null, '11 91111-1111', 1),
-	(null, '11 2266-7842', '11 92222-2222', 2),
-	(null, '11 2462-3485', '11 93333-3333', 3),
-	(null, '11 2214-6458', '11 94444-4444', 4);
+	(null, '11 91111-1111', 1),
+	(null,  '11 92222-2222', 2),
+	(null, '11 93333-3333', 3),
+	(null, '11 94444-4444', 4);
 
 -- INSERT TABELA 
 INSERT INTO estabelecimento VALUES 
@@ -106,12 +101,9 @@ INSERT INTO estabelecimento VALUES
 	(null,'Churrascaria do Renan', 'Rua Kuh xaixange', 78, 'Cidade Tiradentes', 'São Paulo', '08410-410', '62368641000150');
     
 INSERT INTO  telefoneEstabelecimento values
-	(null, '11 2266-7842', null, '11 91111-1111', 1),
-	(null, '11 2266-7842', null, '11 92222-2222', 2),
-	(null, '11 2462-3485', null, '11 93333-3333', 3);
-
-
-
+	(null, '1122667842', null, '11911111111', 1),
+	(null, '1122667842', null, '11922222222', 2),
+	(null, '1124623485', null, '11933333333', 3);
 
 -- Insert do local
 INSERT INTO LocalInstalado VALUES
@@ -126,17 +118,20 @@ INSERT INTO Sensor VALUES
 (null, '2023-07-12', 'Ativo', 3);
 
 select * from Sensor;
-
+DESC CAPTURADADOS;
 -- insert dos dados
 INSERT INTO CapturaDados VALUES
-(1, 1, '2023-10-20 10:00:00', 1),
-(1, 1, '2023-10-20 08:00:00', 2),
-(2, 1, '2023-10-20 10:00:00', 2);
+('2023-10-20 11:00:00', 1, 1),
+('2023-10-20 08:00:00', 1, 1),
+('2023-10-20 11:00:00', 2, 1),
+('2023-10-20 11:30:00', 2, 1),
+('2023-10-20 11:00:00', 3, 1),
+('2023-10-20 11:10:00', 2, 1);
 
 -- Aqui está acontecendo um select mais "refinado", peguei para puxar apenas o id, valor da captura, o id do sensor
 -- e o local em q ele foi instalado, eu fiz um join na tabela sensor e por la, eu pego o local em que o sensor foi instalado.
 
-SELECT idCaptura, valor, dtHora, idSensor, localInstalado, nomeestabelecimento FROM CapturaDados 
+SELECT dtHora, valor, dtHora, idSensor, localInstalado, nomeestabelecimento FROM CapturaDados 
 	JOIN Sensor
 		ON CapturaDados.fk_sensor = Sensor.idSensor
 			JOIN LocalInstalado ON sensor.fk_local_instalado = LocalInstalado.idLocal
