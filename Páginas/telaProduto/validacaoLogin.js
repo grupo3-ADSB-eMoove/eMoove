0773f051ba50eab5ac65e81b614ce6fd13bf9dc8
KauanSantosJS <KauanJesus@sptech.school>
2023-04-23 20:23:49 -0300

    implementando o botão de ocultar senha

--- Páginas/telaProduto/validacaoLogin.js
+++ Páginas/telaProduto/validacaoLogin.js
@@ -53,3 +53,17 @@ function efetuarLogin() {
         })
     }
 }
+const password = document.getElementById("ipt_loginSenha")
+const icone = document.getElementById("icone")
+
+function showHide(){
+    if(password.type === 'password'){
+        password.setAttribute('type', 'text');
+        icone.classList.add("hide")
+
+    }else{
+    password.setAttribute('type', 'password');
+    icone.classList.remove('hide')
+    }
+
+}
