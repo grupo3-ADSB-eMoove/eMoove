d54b7b1385fb2da79d85bc49c2151b03d4e8e803
Renan2805 <renansilva.dev@gmail.com>
2023-05-06 06:54:17 -0300

    Tela de contratação feita do zero

--- Páginas/contratação/script.js
+++ Páginas/contratação/script.js
@@ -1,235 +1,286 @@
-const nomeEst = document.querySelector('#input_nome');
-const cnpj = document.querySelector('#ipt_cnpj');
-const area = document.querySelector('#input_area')
-const cep = document.querySelector('#input_CEP');
-const bairro = document.querySelector('#input_bairro');
-const endereco = document.querySelector('#input_endereco');
-const numero = document.querySelector('#input_numero');
-const nome = document.querySelector('#input_nomeUsuario')
-const sobrenome = document.querySelector('#input_sobrenome')
-const email = document.querySelector('#input_email');
-const senha = document.querySelector('#input_senha')
+const form = document.getElementById("form-contrato");
 
+const iptNomeFantasia = form[0];
+const iptCNPJ = form[1];
+const iptArea = form[2];
+const iptCEP = form[3];
+const iptBairro = form[4];
+const iptLogradouro = form[5];
+const iptNumero = form[6];
 
+const iptNomeUsuario = form[7];
+const iptSobrenomeUsuario = form[8];
+const iptEmail = form[9];
+const iptSenha = form[10];
+const iptConfirmaSenha = form[11];
 
+document.getElementById("btn-contratar").addEventListener("click", (e) => {
+  e.preventDefault();
+  var nomeFantasiaValidado = validarNomeFantasia();
+  var cnpjValidado = validarCNPJ();
+  var areaValidada = validarArea();
+  var cepValidado = validarCEP();
+  var numeroValidado = validarNumero();
 
-function validarContrato() {
-  const spanErroCep = document.getElementById('mensagemErroCEP')
-  const spanErroEmail = document.getElementById('mensagemErroEmail')
-  const spanErroSenha = document.getElementById('mensagemErroSenha')
- 
-    var usuario = email.value.substr(0, email.value.indexOf('@'))
-    var dominio = email.value.substr(email.value.indexOf('@') + 1, email.value.length) 
-  
-  
+  var nomeUsuarioValidado = validarNomeUsuario();
+  var sobrenomeusuarioValidado = validarSobrenomeUsuario();
 
-  var validacaoEmail = 
-(usuario.length >=1) &&
-(dominio.length >=3) &&
-(usuario.search("@")==-1) &&
-(dominio.search("@")==-1) &&
-(usuario.search(" ")==-1) &&
-(dominio.search(" ")==-1) &&
-(dominio.search(".")!= -1) &&
-(dominio.indexOf(".") >=1)&&
-(dominio.lastIndexOf(".") < dominio.length - 1) 
+  var senhaValidada = validarSenha();
 
- var validacaoSenha =
- (senha.value.length < 6) ||
- (senha.value.toUpperCase() == senha.value) ||
- (senha.value.toLowerCase() == senha.value) ||
- (senha.value.indexOf('!') == -1) &&
- (senha.value.indexOf('@') == -1) &&
- (senha.value.indexOf('#') == -1) &&
- (senha.value.indexOf('$') == -1) &&
- (senha.value.indexOf('%') == -1) &&
- (senha.value.indexOf('&') == -1) &&
- (senha.value.indexOf('?') == -1) &&
- (senha.value.indexOf('-') == -1) &&
- (senha.value.indexOf('_') == -1) &&
- (senha.value.indexOf('0') == -1) &&
- (senha.value.indexOf('1') == -1) &&
- (senha.value.indexOf('2') == -1) &&
- (senha.value.indexOf('3') == -1) &&
- (senha.value.indexOf('4') == -1) &&
- (senha.value.indexOf('5') == -1) &&
- (senha.value.indexOf('6') == -1) &&
- (senha.value.indexOf('7') == -1) &&
- (senha.value.indexOf('8') == -1) &&
- (senha.value.indexOf('9') == -1) 
-
-
-
-  if (cep.value.length != 9 && cep.value != '') {
-   spanErroCep.style.display = 'block'
-  } 
-  else if (cep.value == ''  || email.value == '' || cnpj.value == '' || nomeEst.value == '' || numero.value == '' || bairro.value == '' || endereco.value == '' || senha.value == '' || area.value <= 0) {
-   alert("Os campos não podem ser vazios")}
-  else if (!validacaoEmail) {
-    spanErroEmail.style.display = 'block'
-    }else if(validacaoSenha){
-      spanErroSenha.style.display = 'block'
-    }
-      else{
-    var blur = document.getElementById('blur')
-    blur.classList.toggle('active')
-    var popup = document.getElementById('popup')
-    popup.classList.toggle('active')
+  if (
+    nomeFantasiaValidado &&
+    cnpjValidado &&
+    areaValidada &&
+    cepValidado &&
+    numeroValidado &&
+    nomeUsuarioValidado &&
+    sobrenomeusuarioValidado &&
+    senhaValidada
+  ) {
+    alert("Contrato Realizado com Sucesso");
   }
+});
+
+function validarNomeFantasia() {
+  const nome = iptNomeFantasia.value;
+  const msgErro = document.getElementById("msg-erro-nome-fantasia");
+
+  if (nome.length < 1) {
+    msgErro.innerText = "Nome inválido";
+    iptNomeFantasia.style.borderColor = "red";
+    return false;
+  }
+
+  msgErro.innerText = "";
+  iptNomeFantasia.style.borderColor = "#dc7424";
+  return true;
 }
 
+function validarCNPJ() {
+  const cnpj = iptCNPJ.value;
+  const msgErro = document.getElementById("msg-erro-cnpj");
 
+  if (cnpj.length < 18) {
+    msgErro.innerText = "CNPJ inválido";
+    iptCNPJ.style.borderColor = "red";
+    return false;
+  }
 
+  msgErro.innerText = "";
+  iptCNPJ.style.borderColor = "#dc7424";
 
+  return true;
+}
 
+function validarArea() {
+  const area = Number(iptArea.value);
+  const msgErro = document.getElementById("msg-erro-area");
 
+  if (area <= 0) {
+    msgErro.innerText = "Área inválida";
+    iptArea.style.borderColor = "red";
+    return false;
+  }
 
+  msgErro.innerText = "";
+  iptArea.style.borderColor = "#dc7424";
+  return true;
+}
+
+function validarCEP() {
+  const cep = iptCEP.value;
+  const msgErro = document.getElementById("msg-erro-cep");
+  
+  if (cep.length != 9) {
+    iptCEP.style.borderColor = "red";
+    msgErro.innerText = "CEP inválido";
+    return false;
+  }
+  
+  msgErro.innerText = "";
+  iptCEP.style.borderColor = "#dc7424";
+
+  return true;
+}
+
+function validarNumero() {
+  const numero = Number(iptNumero.value);
+  const msgErro = document.getElementById("msg-erro-numero");
+
+  if (numero == null || numero <= 0) {
+    msgErro.innerText = "Numero inválido";
+    iptNumero.style.borderColor = "red";
+    return false;
+  }
+
+  msgErro.innerText = "";
+  iptNumero.style.borderColor = "#dc7424";
+  return true;
+}
+
+function validarNomeUsuario() {
+  const nome = iptNomeUsuario.value;
+  const msgErro = document.getElementById("msg-erro-nome-usuario");
+
+  if (nome.length < 3) {
+    msgErro.innerText = "Nome inválido";
+    iptNomeUsuario.style.borderColor = "red";
+    return false;
+  }
+  msgErro.innerText = "";
+  iptNomeUsuario.style.borderColor = "#dc7424";
+  return true;
+}
+
+function validarSobrenomeUsuario() {
+  const nome = iptSobrenomeUsuario.value;
+  const msgErro = document.getElementById("msg-erro-sobrenome-usuario");
+
+  if (nome.length < 3) {
+    msgErro.innerText = "Nome inválido";
+    iptSobrenomeUsuario.style.borderColor = "red";
+    return false;
+  }
+  msgErro.innerText = "";
+  iptSobrenomeUsuario.style.borderColor = "#dc7424";
+  return true;
+}
+
+function validarEmail() {}
+
+function validarSenha() {
+  const msgErro = document.getElementById("msg-erro-senha");
+
+  var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var letrasMin = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var letrasMai = letrasMin.map((letra) => letra.toUpperCase());
+  var especiais = ["!", "@", "#", "$", "%", "&", "?", "-", "_"];
+
+  const senha = iptSenha.value;
+  const confirmaSenha = iptConfirmaSenha.value;
+
+  var senhaTemNumero = numeros.some((n) => senha.includes(n));
+  var senhaTemLetraMin = letrasMin.some((l) => senha.includes(l));
+  var senhaTemLetraMai = letrasMai.some((l) => senha.includes(l));
+  var senhaTemEspecial = especiais.some((e) => senha.includes(e));
+
+  if (senha.length < 8 || senha.length > 16) {
+    msgErro.innerText = "senha deve ter entre 8 e 16 caracteres";
+    iptSenha.style.borderColor = "red";
+    return false;
+  }
+
+  if (!senhaTemNumero) {
+    msgErro.innerText = "senha deve conter pelo menos um numero";
+    iptSenha.style.borderColor = "red";
+    return false;
+  }
+
+  if (!senhaTemLetraMin) {
+    msgErro.innerText = "senha deve conter pelo menos uma letra minúscula";
+    iptSenha.style.borderColor = "red";
+    return false;
+  }
+
+  if (!senhaTemLetraMai) {
+    msgErro.innerText = "senha deve conter pelo menos uma letra maiúscula";
+    iptSenha.style.borderColor = "red";
+    return false;
+  }
+
+  if (!senhaTemEspecial) {
+    msgErro.innerText = "senha deve conter pelo menos um caractér especial";
+    iptSenha.style.borderColor = "red";
+    return false;
+  }
+
+  if (senha != confirmaSenha) {
+    msgErro.innerText = "senhas não são iguais";
+    iptSenha.style.borderColor = "red";
+    iptConfirmaSenha.style.borderColor = "red";
+    return false;
+  }
+
+  return true;
+}
 
 // VALIDAÇÃO CEP ERRO E IMPLEMENTAÇÃO CEP
+iptCEP.addEventListener("keyup", (e) => {
+  const cep = iptCEP.value;
 
-document.getElementById('input_CEP').addEventListener('keyup', exibirEndereco)
-function exibirEndereco() {
-  let cep = document.querySelector(`#input_CEP`).value;
-  if (cep.length != 9) { 
-    document.getElementById('input_endereco').value = ''
-    document.getElementById('input_bairro').value = ''
+  var numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  if (!numeros.includes(e.key)) {
+    e.preventDefault();
+  }
+
+  if (cep.length != 9) {
+    iptLogradouro.value = "";
+    iptBairro.value = "";
     return;
   }
- 
+
   let url = `https://viacep.com.br/ws/${cep}/json/`;
-  fetch(url).then(function (response) {
-    response.json().then(mostrarEndereco);
+  fetch(url).then((response) => {
+    response.json().then((dados) => {
+      if (dados.erro) {
+        iptCEP.style.borderColor = "red";
+        msgErro.innerText = "CEP inválido";
+      } else {
+        iptCEP.style.borderColor = "#dc7424";
+        iptLogradouro.value = dados.logradouro;
+        iptBairro.value = dados.bairro;
+      }
+    });
   });
-}
+});
 
-function mostrarEndereco(dados) {
-  let cep = document.querySelector(`#input_CEP`).value;
-  const spanErroCep = document.getElementById('mensagemErroCEP')
-  if (dados.erro) {
-    // alert('Não foi possível localizar endereço!');
+iptCEP.addEventListener("keypress", (_e) => {
+  const cepLength = iptCEP.value.length;
+  if (cepLength == 5) iptCEP.value += "-";
+});
 
-      spanErroCep.style.display = 'block'
+iptCNPJ.addEventListener("keypress", (e) => {
+  var value = iptCNPJ.value;
 
-  } else {
-    document.getElementById('input_endereco').value = dados.logradouro
-    document.getElementById('input_bairro').value = dados.bairro
-    spanErroCep.style.display = 'none'
-  }
-}
+  var numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-
-
-cnpj.addEventListener('keypress', (e) => {
-  var value = cnpj.value;
-
-  var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-  if(!numeros.includes(e.key)) {
-    e.preventDefault()
+  if (!numeros.includes(e.key)) {
+    e.preventDefault();
   }
 
-  if(value.length == 2 || value.length == 6) {
-    cnpj.value += '.'
+  if (value.length == 2 || value.length == 6) {
+    iptCNPJ.value += ".";
   } else if (value.length == 10) {
-    cnpj.value += '/'
-  } else if(value.length == 15) {
-    cnpj.value += '-'
+    iptCNPJ.value += "/";
+  } else if (value.length == 15) {
+    iptCNPJ.value += "-";
   }
-})
-
-
-cep.addEventListener('keypress', () => {
-  let cepLength = cep.value.length;
-if (cepLength == 5) {
-    cep.value += '-';
-  }
-})
-
-
-
-
-
-  email.addEventListener ('keypress' ,()=>{
-
-  const spanErroEmail = document.getElementById('mensagemErroEmail')
-  const spanErroSenha = document.getElementById('mensagemErroSenha')
- 
-    var usuario = email.value.substr(0, email.value.indexOf('@'))
-    var dominio = email.value.substr(email.value.indexOf('@') + 1, email.value.length) 
-
-            var validacaoEmail = 
-          (usuario.length >=1) &&
-          (dominio.length >=3) &&
-          (usuario.search("@")==-1) &&
-          (dominio.search("@")==-1) &&
-          (usuario.search(" ")==-1) &&
-          (dominio.search(" ")==-1) &&
-          (dominio.search(".")!= -1) &&
-          (dominio.indexOf(".") >=1)&&
-          (dominio.lastIndexOf(".") < dominio.length - 1) 
-
-          if (validacaoEmail) {
-            spanErroEmail.style.display = 'none'
-          }
-  })
-
-
-  senha.addEventListener ('keypress' ,()=>{
-
-    const spanErroSenha = document.getElementById('mensagemErroSenha')
-   
-    var validacaoSenha =
- (senha.value.length < 6) ||
- (senha.value.toUpperCase() == senha.value) ||
- (senha.value.toLowerCase() == senha.value) ||
- (senha.value.indexOf('!') == -1) &&
- (senha.value.indexOf('@') == -1) &&
- (senha.value.indexOf('#') == -1) &&
- (senha.value.indexOf('$') == -1) &&
- (senha.value.indexOf('%') == -1) &&
- (senha.value.indexOf('&') == -1) &&
- (senha.value.indexOf('?') == -1) &&
- (senha.value.indexOf('-') == -1) &&
- (senha.value.indexOf('_') == -1) &&
- (senha.value.indexOf('0') == -1) &&
- (senha.value.indexOf('1') == -1) &&
- (senha.value.indexOf('2') == -1) &&
- (senha.value.indexOf('3') == -1) &&
- (senha.value.indexOf('4') == -1) &&
- (senha.value.indexOf('5') == -1) &&
- (senha.value.indexOf('6') == -1) &&
- (senha.value.indexOf('7') == -1) &&
- (senha.value.indexOf('8') == -1) &&
- (senha.value.indexOf('9') == -1) 
-  
-            if (validacaoSenha) {
-              spanErroSenha.style.display = 'none'
-            }
-    })
-
-
-
-
-
-
-
-
-
-// ICONE MOSTRAR SENHA 
-const password = document.getElementById("input_senha")
-const icone = document.getElementById("icone")
-
-function showHide(){
-    if(password.type === 'password'){
-        password.setAttribute('type', 'text');
-        icone.classList.add("hide")
-
-    }else{
-    password.setAttribute('type', 'password');
-    icone.classList.remove('hide')
-    }
-
-}
-
-
-
+});
