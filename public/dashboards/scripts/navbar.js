198d37ce2c7140cec63f7f16dfc32200e6b90b9f
leopls07 <leonardobento233@gmail.com>
2023-05-26 14:51:50 -0300

    Acertos na dashboard + Links, Imagens e função de sair

--- public/dashboards/scripts/navbar.js
+++ public/dashboards/scripts/navbar.js
@@ -9,3 +9,35 @@ menuButton.addEventListener('click', () => {
     element.classList.toggle('hidden')
   })
 })
+
+
+
+const imgSair = document.querySelector('#btn-sair-nav')
+
+const div_confirmacao_sair = document.querySelector('.wrapper-confirmacao')
+const btn_sair = document.querySelector('#btn-sair')
+const btn_cancelar = document.querySelector('#btn-cancelar')
+
+imgSair.addEventListener('click',()=>{
+
+  div_confirmacao_sair.style.opacity = '1'
+  div_confirmacao_sair.style.display = 'block'
+
+})
+
+
+btn_sair.addEventListener('click',()=>{
+
+  div_confirmacao_sair.style.display = 'none'
+  
+
+  sessionStorage.clear()
+ 
+      window.location.href = '../index.html'
+  
+})
+
+btn_cancelar.addEventListener('click',()=>{
+  div_confirmacao_sair.style.display = 'none'
+  
+})
